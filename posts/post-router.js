d654981e320c2bc73bce30645b19b03ee28e5804
josephriosio <blankworldwide@gmail.com>
2019-04-23 12:58:21 -0700

    added get all post endpoint

--- posts/post-router.js
+++ posts/post-router.js
@@ -19,4 +19,13 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get("/", async (req, res) => {
+  try {
+    const getPosts = await db.find();
+    res.status(200).json(getPosts);
+  } catch (err) {
+    res.status(500).json("The posts information could not be retrieved.");
+  }
+});
+
 module.exports = router;
